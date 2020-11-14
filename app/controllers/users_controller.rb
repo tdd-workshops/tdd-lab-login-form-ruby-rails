45903def579117a2a95359fff4e321d0cc3f4c73
Michael Cheng <michael_cheng@tech.gov.sg>
2020-11-14 19:51:31 +0800

    Add base templates.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def create
+
+  end
+
+  def welcome
+
+  end
+
+  def logout
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'login#index'
+
+  resources :users, only: [:create] do
+    collection do
+      get 'welcome'
+      get 'logout'
+    end
+  end
 end
