d65d9429fedff181a8dd14944cefa6076c4a4cc9
Michael Cheng <michael_cheng@tech.gov.sg>
2020-11-23 12:54:14 +0800

    Refactor how we validate matching password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,22 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+  validate :matching_password_confirmation, on: :signup
 
   validates_presence_of :password, on: :create
 
   def self.signup(new_user)
-    if new_user.password != new_user.password_confirm
-      new_user.errors.add(:base, :mismatch_password, message: 'Passwords do not match')
-    end
-
+    new_user.valid?(:signup)
     new_user.save if new_user.errors.empty?
 
     new_user
+  end
+
+  private
+
+  def matching_password_confirmation
+    if password_confirm && password != password_confirm
+      errors.add(:base, :mismatch_password, message: 'Passwords do not match')
+    end
   end
 end
